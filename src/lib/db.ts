4848adfaf0d1664c3a485197944adbaf9c8947ad
Jeremy Nyangi <justinjerome401@gmail.com>
2025-10-15 16:59:31 +0300

    database set up

---
+++ src/lib/db.ts
@@ -0,0 +1,13 @@
+// https://www.prisma.io/docs/orm/more/help-and-troubleshooting/nextjs-help#recommended-prisma-client-setup
+
+import { PrismaClient } from "@/generated/prisma";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
