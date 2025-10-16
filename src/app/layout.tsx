faae991bd5df9afe0ed6b61bd992dc666f474d58
Jeremy Nyangi <justinjerome401@gmail.com>
2025-10-16 03:11:43 +0300

    theme and styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${outfit.className} antialiased`}
+        className={`${outfit.className} dark antialiased`}
       >
         <Providers>
           {children}
