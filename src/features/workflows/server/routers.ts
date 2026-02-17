1614daddf33b813b0644c76cc3cb9ccee0a757be
Jeremy Nyangi <justinjerome401@gmail.com>
2026-02-18 02:05:12 +0300

    node executions

--- src/features/workflows/server/routers.ts
+++ src/features/workflows/server/routers.ts
@@ -5,8 +5,29 @@ import prisma from "@/lib/db";
 import { createTRPCRouter, premiumProcedure, protectedProcedure } from "@/trpc/init";
 import { generateSlug } from "random-word-slugs";
 import z from "zod";
+import { inngest } from "@/inngest/client";
 
 export const workflowsRouter = createTRPCRouter({
+
+    execute: protectedProcedure
+        .input(z.object({ id: z.string() }))
+        .mutation(async ({ input, ctx }) => {
+            const workflow = await prisma.workflow.findUniqueOrThrow({
+                where: {
+                    id: input.id,
+                    userId: ctx.auth.user.id
+                }
+            })
+
+            await inngest.send({
+                name: "workflows/execute.workflow",
+                data: {
+                    workflowId: input.id
+                }
+            })
+
+            return workflow
+        }),
 
     create: premiumProcedure.mutation(({ ctx }) => {
         return prisma.workflow.create({
@@ -103,7 +124,7 @@ export const workflowsRouter = createTRPCRouter({
 
                 await tx.workflow.update({
                     where: { id },
-                    data: { updatedAt: new Date()}
+                    data: { updatedAt: new Date() }
                 })
 
                 return workflow
