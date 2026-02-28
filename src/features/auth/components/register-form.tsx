0abfd1dff75612120f30d173a78af112f9fbdd8f
Jeremy Nyangi <justinjerome401@gmail.com>
2026-02-28 03:29:59 +0300

    social sign in

--- src/features/auth/components/register-form.tsx
+++ src/features/auth/components/register-form.tsx
@@ -40,6 +40,32 @@ export function RegisterForm() {
         },
     })
 
+    const signInGithub = async () => {
+            await authClient.signIn.social({
+                provider: "github"
+            }, {
+                onSuccess: () => {
+                    router.push("/")
+                },
+                onError: () => {
+                    toast.error("Something went wrong")
+                } 
+            },)
+        }
+    
+        const signInGoogle = async () => {
+            await authClient.signIn.social({
+                provider: "google"
+            }, {
+                onSuccess: () => {
+                    router.push("/")
+                },
+                onError: () => {
+                    toast.error("Something went wrong")
+                } 
+            },)
+        }
+
     const onSubmit = async (values: RegisterFormValues) => {
         await authClient.signUp.email(
             {
@@ -77,11 +103,11 @@ export function RegisterForm() {
                         <form onSubmit={form.handleSubmit(onSubmit)}>
                             <div className="grid gap-6">
                                 <div className="flex flex-col gap-4">
-                                    <Button variant="outline" className="w-full" type="button" disabled={isPending}>
+                                    <Button variant="outline" className="w-full" type="button" disabled={isPending} onClick={signInGithub}>
                                         <Image src="/logos/github.svg" width={20} height={20} alt="GitHub" />
                                         Continue with GitHub
                                     </Button>
-                                    <Button variant="outline" className="w-full" type="button" disabled={isPending}>
+                                    <Button variant="outline" className="w-full" type="button" disabled={isPending} onClick={signInGoogle}>
                                         <Image src="/logos/google.svg" width={20} height={20} alt="Google" />
                                         Continue with Google
                                     </Button>
